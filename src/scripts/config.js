e3d1abe6ef5c4b9153fd447e4cc4d11af5678542
cindia hua <xinranh2@illinois.edu>
2022-05-09 22:22:12 -0500

    Revert "Created custom nodes necessary to create experiment"
    This reverts commit 7660a2be99525824a0af887472cbd4bf801c062a.

--- src/scripts/config.js
+++
@@ -1,8 +0,0 @@
-const Config = {
-    ROSBRIDGE_SERVER_IP: "10.192.233.101",
-    ROSBRIDGE_SERVER_PORT: "9090",
-    RECONNECTION_TIMER: 3000,
-    CMD_VEL_TOPIC: "/test",
-}
-
-export default Config
