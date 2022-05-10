d8f5ef77ea1d2787d1d41d7b3c2107a500c75c32
cindia hua <xinranh2@illinois.edu>
2022-05-09 22:06:50 -0500

    Created custom nodes necessary to create experiment

---
+++ src/components/node-types/HeadControlNode.jsx
@@ -0,0 +1,60 @@
+import { useCallback } from 'react';
+import React, { useState } from 'react';
+import { Handle, Position } from 'react-flow-renderer';
+
+import '../../text-updater-node.css';
+
+function HeadControlNode({ data }) {
+
+  const [startAngle, setStartAngle] = useState(0);
+  const onStartAngleChange = useCallback((evt) => {
+    setStartAngle(evt.target.value);
+    data.startAngle = evt.target.value
+  }, []);
+  const [endAngle, setEndAngle] = useState(360);
+  const onEndAngleChange = useCallback((evt) => {
+    setEndAngle(evt.target.value);
+    data.endAngle = evt.target.value
+  }, []);
+  const [degrees, setDegrees] = useState(0);
+  const onDegreeChange = useCallback((evt) => {
+    console.log(evt.target.value);
+    setDegrees(evt.target.value);
+    data.degrees = evt.target.value
+  }, []);
+
+  const [direction, setDirection] = useState("cw")
+  const onDirectionChange = useCallback((evt) => {
+    setDirection(evt.target.value);
+    data.direction = evt.target.value
+  }, []);
+
+  return (
+    <div className="text-updater-node">
+      <Handle type="target" position={Position.Top} />
+      <h6>Head Controller</h6>
+        <form>
+        {/* <label htmlFor="text">Start Angle:</label>
+        <input id="text" name="text" value={startAngle} onChange={onStartAngleChange} />
+        <br />
+        <br />
+        <label htmlFor="text"> End Angle:</label>
+        <input id="text" name="text" value={endAngle} onChange={onEndAngleChange} />
+        <br />
+        <br /> */}
+        <label htmlFor="text">Degrees per step:</label>
+        <input id="text" name="text" value={degrees} onChange={onDegreeChange} />
+        <br />
+        <br />
+        <label htmlFor="selector">Direction:</label>
+        <select value={direction} onChange={onDirectionChange} >
+          <option value="cw">Clockwise</option>
+          <option value="ccw">Counterclockwise</option>
+        </select>
+        </form>
+      <Handle type="source" position={Position.Bottom} />
+    </div>
+  );
+}
+
+export default HeadControlNode;
---
+++ src/components/node-types/Nodetypes.jsx
@@ -0,0 +1,11 @@
+export const nodeTypesMap = {
+    'headControl' : 'Head Control Node',
+    'railControl' : 'Rail Control Node',
+    'playRecord' : 'Play Record Node',
+    'forLoop' : 'For Loop Node'
+}
+
+export const robotNodeTypes = [
+    'head',
+    'rail'
+]
